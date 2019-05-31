7afd8037e3f0a89862cd1630cc72a2ec1c322200
Omar1123 <omar1123veliz@gmail.com>
2019-05-30 20:00:54 -0600

    ADD: Real time with commands:

---
+++ core/server/realtime.js
@@ -0,0 +1,29 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+let health = [
+];
+
+async function getCommand(command) {
+
+  const { stdout, stderr } = await exec(command);
+
+  if(stderr) {
+    return await ' '
+  } else {
+    return await stdout
+  }
+}
+
+module.exports = async(req, res) => {
+
+  let commandLine = await getCommand(req.body.command)
+
+  health.push({
+    "result" : commandLine
+  });
+
+  console.log('Finish real time')
+  res.status(200).json(health)
+  health = [];
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router()
 const health = require('../core/server/healthCheck.js')
+const command = require('../core/server/realtime.js')
 
 routes.post('/server/health/check', health);
+routes.post('/server/health/command', command);
 
 module.exports = routes;
